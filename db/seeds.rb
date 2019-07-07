bc0baa8a746b18f7cbb3fae9bc4cfb9fad4f416b
Iwona <iwona.kruczaj11@gmail.com>
2019-07-07 12:27:45 +0200

    Add positions in Species (file seeds.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Species.create(name: 'Kaktus', latin_name: 'Caclaceae', irrigation_level: :medium)
+Species.create!(name: 'Storczyk', latin_name: 'Orchis', irrigation_level: :medium)
+Species.create!(name: 'Paprotka doniczkowa', latin_name: 'Nephrolepis', irrigation_level: :medium)
+Species.create!(name: 'Aloes', latin_name: 'Aloe', irrigation_level: :low)
+Species.create!(name: 'Daktylowiec kanaryjski', latin_name: 'Phoenix', irrigation_level: :medium)
+Species.create!(name: 'Fikus', latin_name: 'Ficus', irrigation_level: :medium)
+Species.create!(name: 'Figowiec benajmina', latin_name: 'Ficus benjamina', irrigation_level: :medium)
+Species.create!(name: 'Pelargonia', latin_name: 'Pelargonium', irrigation_level: :low)
+Species.create!(name: 'Agawa', latin_name: 'Agave', irrigation_level: :low)
+Species.create!(name: 'Bonsai', irrigation_level: :high)
+
+
