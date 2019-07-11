8f54f86b6afb873c0a1a02e19e7d46a96e526003
Filip <placzek.filip@gmail.com>
2019-07-11 18:44:07 +0200

    little cosmetic changes in waterme html and in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'plants#waterme'
-  
+  root to: 'plants#waterme'  
 end
