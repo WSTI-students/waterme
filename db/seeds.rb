a0b9ab2a5f2e0b27173123eb66c85a4d046a897c
Filip <placzek.filip@gmail.com>
2019-07-11 17:05:22 +0200

    add ! to find or create by

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Species.find_or_create_by(name: 'Kaktus', latin_name: 'Caclaceae', irrigation_level: :medium)
+Species.find_or_create_by!(name: 'Kaktus', latin_name: 'Caclaceae', irrigation_level: :medium)
 Species.find_or_create_by!(name: 'Storczyk', latin_name: 'Orchis', irrigation_level: :medium)
 Species.find_or_create_by!(name: 'Paprotka doniczkowa', latin_name: 'Nephrolepis', irrigation_level: :medium)
 Species.find_or_create_by!(name: 'Aloes', latin_name: 'Aloe', irrigation_level: :low)
@@ -16,4 +16,4 @@ Species.find_or_create_by!(name: 'Figowiec benajmina', latin_name: 'Ficus benjam
 Species.find_or_create_by!(name: 'Pelargonia', latin_name: 'Pelargonium', irrigation_level: :low)
 Species.find_or_create_by!(name: 'Agawa', latin_name: 'Agave', irrigation_level: :low)
 Species.find_or_create_by!(name: 'Bonsai', irrigation_level: :high)
-Species.find_or_create_by!(name: 'Bluszcz', latin_name: 'Hedera Helix', irrigation_level: medium) 
+Species.find_or_create_by!(name: 'Bluszcz', latin_name: 'Hedera Helix', irrigation_level: high) 
