58201e186cba520448d2c716a08fb5f62647a492
sandra_fojcik <sanfoj95@gmail.com>
2019-07-12 16:29:55 +0200

    Add 2 plants to user plants list in file seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Lista gatunków w aplikacji
 Species.find_or_create_by!(name: 'Kaktus', latin_name: 'Caclaceae', irrigation_level: :medium)
 Species.find_or_create_by!(name: 'Storczyk', latin_name: 'Orchis', irrigation_level: :medium)
 Species.find_or_create_by!(name: 'Paprotka doniczkowa', latin_name: 'Nephrolepis', irrigation_level: :medium)
@@ -16,4 +17,8 @@ Species.find_or_create_by!(name: 'Figowiec benajmina', latin_name: 'Ficus benjam
 Species.find_or_create_by!(name: 'Pelargonia', latin_name: 'Pelargonium', irrigation_level: :low)
 Species.find_or_create_by!(name: 'Agawa', latin_name: 'Agave', irrigation_level: :low)
 Species.find_or_create_by!(name: 'Bonsai', irrigation_level: :high)
-Species.find_or_create_by!(name: 'Bluszcz', latin_name: 'Hedera Helix', irrigation_level: high) 
+Species.find_or_create_by!(name: 'Bluszcz', latin_name: 'Hedera Helix', irrigation_level: :high) 
+
+# Lista naszych roślinek dodanych do aplikacji
+Plant.find_or_create_by!(name: 'Paprotka na oknie', irrigation_frequency_in_days: 2, health_status: :normal, species: Species.third)
+Plant.find_or_create_by!(name: 'Storczyk Jan', irrigation_frequency_in_days: 4, health_status: :bloom, species: Species.second)
